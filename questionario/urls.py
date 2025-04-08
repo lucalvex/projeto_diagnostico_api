3c66f1162474eba534fb75e7022469dfcea2f7d6
Caio Arasaki <sasazaki08@gmail.com>
2025-04-08 19:52:51 -0300

    Corrigido

--- questionario/urls.py
+++ questionario/urls.py
@@ -2,11 +2,9 @@ from django.urls import path
 from .views import (
     QuestionarioView,
     EnviarRespostasView,
-    ResultadosUsuarioView
 )
 
 urlpatterns = [
     path('questionario/', QuestionarioView.as_view(), name='obter-questionario'),
     path('respostas/', EnviarRespostasView.as_view(), name='enviar-respostas'),
-    path('resultados/<int:usuario_id>/', ResultadosUsuarioView.as_view(), name='resultados-usuario'),
 ]
--- questionario/views.py
+++ questionario/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import AllowAny
 from django.db import transaction
 from django.http import JsonResponse
 from users.models import UserAccount
-from .models import Secao, Pergunta, RespostaSecao, RespostaPergunta
+from .models import Modulo, Dimensao, Pergunta, RespostaDimensao, RespostaPergunta
 import json
 
 class QuestionarioView(APIView):
@@ -13,28 +13,42 @@ class QuestionarioView(APIView):
 
     def get(self, request):
         try:
-            secoes = Secao.objects.prefetch_related('perguntas').all()
-            print(secoes)
-            dados_questionario = []
 
-            for secao in secoes:
-                perguntas = secao.perguntas.all()
+            modulos = Modulo.objects.prefetch_related('dimensoes__perguntas').all()
+
+            dadosQuestionario = []
+
+            for modulo in modulos:
+                dimensoesDoModulo = modulo.dimensoes.all()
+                dadosDimensoes = []
+
+                for dimensao in dimensoesDoModulo:
+                    perguntas = dimensao.perguntas.all()
+                    
+                    dadosDimensao = {
+                        'dimensaoTitulo': dimensao.titulo,
+                        'descricao': dimensao.descricao,
+                        'tipo': dimensao.get_tipo_display(),
+                        'perguntas': [
+                            {   
+                                'id': p.id,
+                                'pergunta': p.pergunta,
+                                'explicacao': p.explicacao,
+                            } for p in perguntas
+                        ]
+                    }
+                    dadosDimensoes.append(dadosDimensao)
                 
-                dados_secao = {
-                    'secao_titulo': secao.titulo,
-                    'descricao': secao.descricao,
-                    'tipo': secao.get_tipo_display(),
-                    'perguntas': [
-                        {
-                            'id_na_secao': p.id_secao,
-                            'pergunta': p.pergunta,
-                            'explicacao': p.explicacao,
-                        } for p in perguntas
-                    ]
+                dadosModulo = {
+                    'nome': modulo.nome,
+                    'descricao': modulo.descricao,
+                    'tempo': modulo.tempo, 
+                    'perguntasQntd': modulo.perguntasQntd,
+                    'dimensoes': dadosDimensoes
                 }
-                dados_questionario.append(dados_secao)
+                dadosQuestionario.append(dadosModulo)
             
-            return Response({'secoes': dados_questionario})
+            return Response({'modulos': dadosQuestionario})
         
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -44,25 +58,25 @@ class EnviarRespostasView(APIView):
     def post(self, request):
         try:
             data = request.data
-            usuario_id = data.get('usuario_id')
+            UsuarioId = data.get('UsuarioId')
             respostas = data.get('respostas')
             
-            if not usuario_id or not respostas:
+            if not UsuarioId or not respostas:
                 return Response({'error': 'Dados incompletos'}, status=status.HTTP_400_BAD_REQUEST)
             
-            usuario = Usuario.objects.get(pk=usuario_id)
+            usuario = Usuario.objects.get(pk=UsuarioId)
             resultados = []
             
-            for secao_id, perguntas_respostas in respostas.items():
-                secao = Secao.objects.get(titulo=secao_id)
+            for dimensaoId, perguntasRespostas in respostas.items():
+                dimensao = dimensao.objects.get(titulo=dimensaoId)
                 valor_final = 0
                 
-                resposta_secao, created = RespostaSecao.objects.get_or_create(
+                respostaDimensao, created = RespostaDimensao.objects.get_or_create(
                     usuario=usuario,
-                    secao=secao
+                    dimensao=dimensao
                 )
                 
-                for pergunta_id, valor in perguntas_respostas.items():
+                for perguntaId, valor in perguntasRespostas.items():
                     valor = int(valor)
                     if not 1 <= valor <= 5:
                         return Response(
@@ -70,21 +84,21 @@ class EnviarRespostasView(APIView):
                             status=status.HTTP_400_BAD_REQUEST
                         )
                     
-                    pergunta = Pergunta.objects.get(pk=pergunta_id)
+                    pergunta = Pergunta.objects.get(pk=perguntaId)
                     
                     RespostaPergunta.objects.update_or_create(
-                        resposta_secao=resposta_secao,
+                        respostaDimensao=respostaDimensao,
                         pergunta=pergunta,
                         defaults={'valor': valor}
                     )
                     
                     valor_final += valor
                 
-                resposta_secao.valor_final = valor_final
-                resposta_secao.save()
+                respostaDimensao.valor_final = valor_final
+                respostaDimensao.save()
                 
                 resultados.append({
-                    'secao_id': secao.titulo,
+                    'dimensaoId': dimensao.titulo,
                     'valor_final': valor_final,
                     'status': 'atualizado' if not created else 'criado'
                 })
@@ -96,31 +110,9 @@ class EnviarRespostasView(APIView):
         
         except Usuario.DoesNotExist:
             return Response({'error': 'Usuário não encontrado'}, status=status.HTTP_404_NOT_FOUND)
-        except Secao.DoesNotExist:
+        except dimensao.DoesNotExist:
             return Response({'error': 'Seção não encontrada'}, status=status.HTTP_404_NOT_FOUND)
         except Pergunta.DoesNotExist:
             return Response({'error': 'Pergunta não encontrada'}, status=status.HTTP_404_NOT_FOUND)
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-class ResultadosUsuarioView(APIView):
-    def get(self, request, usuario_id):
-        try:
-            usuario = Usuario.objects.get(pk=usuario_id)
-            respostas = RespostaSecao.objects.filter(usuario=usuario).select_related('secao')
-            
-            resultados = [
-                {
-                    'secao_id': r.secao.titulo,
-                    'secao_titulo': r.secao.titulo,
-                    'valor_final': r.valor_final,
-                    'data_resposta': r.data_resposta
-                } for r in respostas
-            ]
-            
-            return Response({'resultados': resultados})
-        
-        except Usuario.DoesNotExist:
-            return Response({'error': 'Usuário não encontrado'}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
