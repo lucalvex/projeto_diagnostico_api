2d728c0c05111b74b778831e0aaa7c7016b88983
lucalvex <lucalvex25@gmail.com>
2025-05-06 14:49:20 -0300

    upgrade v.10

--- users/urls.py
+++ users/urls.py
@@ -6,10 +6,11 @@ from .views import (
     LogoutView,
     UserListView
 )
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('jwt/create/', CustomTokenObtainPairView.as_view()),
-    path('jwt/refresh/', CustomTokenRefreshView.as_view()),
+    path('jwt/create/', TokenObtainPairView.as_view()),
+    path('jwt/refresh/', TokenRefreshView.as_view()),
     path('jwt/verify/', CustomTokenVerifyView.as_view()),
     path('logout/', LogoutView.as_view()),
     path('api/users/', UserListView.as_view(), name='user-list'),
