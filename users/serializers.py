4503036302726f6f3db6638b2049652adfdf8866
lucalvex <lucalvex25@gmail.com>
2025-05-06 10:26:49 -0300

    upgrade v.7

--- users/serializers.py
+++ users/serializers.py
@@ -4,4 +4,4 @@ from .models import UserAccount
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         model = UserAccount
-        fields = ('id', 'email', 'username', 'cnpj', 'password', 're_password')
+        fields = ('id', 'email', 'username', 'cnpj', 'cpf', 'password', 're_password')
